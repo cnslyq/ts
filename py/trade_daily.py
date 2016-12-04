88f607425fb5107488621ae56f85fc74b9903b8e
cnslyq <cnslyq@163.com>
2016-12-04 16:03:50 +0800

    Update trade_daily.py

--- py/trade_daily.py
+++ py/trade_daily.py
@@ -9,14 +9,22 @@ if not ts.is_holiday(str(today)):
 
   df = ts.get_index()
   df['date'] = today
-  df.to_sql('trade_today_index',engine,if_exists='append')
+  df.to_sql('trade_today_index', engine, if_exists='append')
   print
   print('trade_today_index done')
 
   df = ts.get_today_all()
   df['date'] = today
-  df.to_sql('trade_today_market',engine,if_exists='append')
+  df.to_sql('trade_today_market', engine, if_exists='append')
   print
   print('trade_today_market done')
+  
+  codes = [item[0] for item in ts.get_area_classified().values]
+  for code in codes:
+    df = ts.get_sina_dd(code, today)
+    df.to_sql('trade_block_trade', engine, if_exists='append')
+  print
+  print('trade_block_trade done')
+  
 else:
   print('Today is a holiday ~')
