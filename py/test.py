8b26916801978d2cdc431b39f3ef8e5f5b84b379
cnslyq <cnslyq@163.com>
2016-12-04 16:04:26 +0800

    Create test.py

---
+++ py/test.py
@@ -0,0 +1,30 @@
+from sqlalchemy import create_engine
+import tushare as ts
+import datetime
+
+today = datetime.date.today()
+print(datetime.datetime.today())
+if ts.is_holiday(str(today)):
+  engine = create_engine('mysql://root:123456@127.0.0.1/mysql?charset=utf8')
+  '''
+  df = ts.get_index()
+  df['date'] = today
+  df.to_sql('trade_today_index', engine, if_exists='append')
+  print
+  print('trade_today_index done')
+
+  df = ts.get_today_all()
+  df['date'] = today
+  df.to_sql('trade_today_market', engine, if_exists='append')
+  print
+  print('trade_today_market done')
+  '''
+  codes = [item[0] for item in ts.get_area_classified().values]
+  for code in codes:
+    df = ts.get_sina_dd(code, today)
+    df.to_sql('trade_block_trade', engine, if_exists='append')
+  print
+  print('trade_block_trade done')
+  
+else:
+  print('Today is a holiday ~')
