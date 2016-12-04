c6ee2763cb02ded189ee3b191add2b067dfc174f
cnslyq <cnslyq@163.com>
2016-12-04 13:33:47 +0800

    Update trade_daily.py

--- py/trade_daily.py
+++ py/trade_daily.py
@@ -2,17 +2,18 @@ from sqlalchemy import create_engine
 import tushare as ts
 import datetime
 
-engine = create_engine('mysql://root:123456@127.0.0.1/mysql?charset=utf8')
 today = datetime.date.today()
+if not ts.is_holiday(str(today)):
+  engine = create_engine('mysql://root:123456@127.0.0.1/mysql?charset=utf8')
 
-df = ts.get_index()
-df['date'] = today
-df.to_sql('trade_today_index',engine,if_exists='append')
-print
-print('trade_today_index done')
+  df = ts.get_index()
+  df['date'] = today
+  df.to_sql('trade_today_index',engine,if_exists='append')
+  print
+  print('trade_today_index done')
 
-df = ts.get_today_all()
-df['date'] = today
-df.to_sql('trade_today_market',engine,if_exists='append')
-print
-print('trade_today_market done')
+  df = ts.get_today_all()
+  df['date'] = today
+  df.to_sql('trade_today_market',engine,if_exists='append')
+  print
+  print('trade_today_market done')
