4c170e97f60912336da37071ebc9c5bee75057e1
cnslyq <cnslyq@163.com>
2016-12-04 16:18:09 +0800

    Update test.py

--- py/test.py
+++ py/test.py
@@ -10,7 +10,7 @@ codes = [item[0] for item in ts.get_area_classified().values]
 print(len(codes))
 for code in codes:
   df = ts.get_sina_dd(code, today)
-  if df is not none:
+  if df is not None:
     df.to_sql('trade_block_trade', engine, if_exists='append')
 print
 print('trade_block_trade done')
