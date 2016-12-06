91ccada8f85e5c388a66931c9b310478dcb8c5c5
cnslyq <cnslyq@163.com>
2016-12-06 20:55:51 +0800

    asdf

--- py/tops_monthly.py
+++
@@ -1,5 +0,0 @@
-import tops_freq
-import pylog as pl
-pl.log("monthly task start...")
-tops_freq.tops(30)
-pl.log("monthly task done")
--- py/tops_weekly.py
+++
@@ -1,5 +0,0 @@
-import tops_freq
-import pylog as pl
-pl.log("weekly task start...")
-tops_freq.tops(5)
-pl.log("weekly task done")
