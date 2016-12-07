97c8776b0f407f835082d9e9cdd03c1a16143e50
cnslyq <cnslyq@163.com>
2016-12-07 15:01:33 +0800

    del

--- conf.py
+++
@@ -1,7 +0,0 @@
-PY_PATH = '/home/ts/py/'
-ENGINE = 'mysql://root:123456@127.0.0.1/mysql?charset=utf8'
-
-INIT_LIST = ['py.stock', 'py.trade']
-DAILY_LIST = ['py.trade']
-WEEKLY_LIST = ['py.tops']
-MONTHLY_LIST = ['py.tops']
