0446f8752de80aea63821ff531c79734f9a5d04f
cnslyq <cnslyq@163.com>
2016-12-28 11:21:42 +0800

    Create tsut.py

---
+++ tsut.py
@@ -0,0 +1,41 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import datetime
+import py.tslog as tsl
+import py.tsconf as tsc
+import sys
+
+engine = create_engine(tsc.ENGINE)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+names = locals()
+def call(mod, func, argv):
+	st = datetime.datetime.today()
+	tsl.log(mod + " " + func + " start...")
+	params = []
+	estr = "method(engine"
+	for param in argv:
+		if param == 's':
+			estr += ", session"
+		elif len(param) == 8:
+			params.append(datetime.date(int(param[0:4]), int(param[4:6]), int(param[6:8])))
+		else:
+			params.append(int(param))
+	for i in range(len(params)):
+		estr += ", params[%i]" % i
+	estr += ")"
+	c = compile(estr,'','exec')
+	obj = __import__(mod, fromlist=True)
+	method = getattr(obj, func)
+	exec c
+	et = datetime.datetime.today()
+	tsl.log(mod + " " + func + " done cost time : " + str(et - st))
+
+if __name__ == "__main__":
+	if len(sys.argv) < 3:
+		print("please input module and func")
+		sys.exit(1)
+	mod = sys.argv[1]
+	func = sys.argv[2]
+	call('py.' + mod, func, sys.argv[3:])
