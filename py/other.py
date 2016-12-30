7fea36ef18cd628af47d6814104391be51e13522
cnslyq <cnslyq@163.com>
2016-12-30 10:58:20 +0800

    test

--- py/other.py
+++ py/other.py
@@ -1,11 +1,12 @@
 import tushare as ts
-import tslog as tsl
+import pylog as pl
 import datetime
 
 def daily(engine, session, cdate):
 	futures_ifs(engine, cdate)
-	global_index(engine, cdate)
-	day_boxoffice(engine, cdate)
+	if cdate.isoweekday() != 7:
+		global_index(engine, cdate)
+		day_boxoffice(engine, cdate)
 
 def history_m(engine, session, year, month):
 	ddate='%i-%i' % (year, month)
@@ -13,7 +14,7 @@ def history_m(engine, session, year, month):
 
 def history_y(engine, session, year):
 	shibor(engine, year)
-	shibor_quote(engine, year)
+	# shibor_quote(engine, year)
 	shibor_ma(engine, year)
 	lpr(engine, year)
 	lpr_ma(engine, year)
@@ -29,114 +30,114 @@ def monthly(engine, session, year, month):
 
 def day_boxoffice(engine, cdate):
 	tbl = "day_boxoffice"
-	tsl.log(tbl + " start...")
+	pl.log(tbl + " start...")
 	try:
 		df = ts.day_boxoffice()
 		df['date'] = cdate - datetime.timedelta(days=1)
 		df.to_sql(tbl,engine,if_exists='append')
-		tsl.log(tbl + " done")
+		pl.log(tbl + " done")
 	except BaseException, e:
 		print e
-		tsl.log(tbl + " error")
+		pl.log(tbl + " error")
 
 def month_boxoffice(engine, ddate=None):
 	tbl = "month_boxoffice"
-	tsl.log(tbl + " start...")
+	pl.log(tbl + " start...")
 	try:
 		df = ts.month_boxoffice(ddate)
 		df['date'] = ddate
 		df.to_sql(tbl,engine,if_exists='append')
-		tsl.log(tbl + " done")
+		pl.log(tbl + " done")
 	except BaseException, e:
 		print e
-		tsl.log(tbl + " error")
+		pl.log(tbl + " error")
 
 def shibor(engine, year, sdate=None):
 	tbl = "shibor"
-	tsl.log(tbl + " start...")
+	pl.log(tbl + " start...")
 	try:
 		df = ts.shibor_data(year)
 		if sdate is not None:
 			df = df[df.date >= sdate]
 		df.to_sql(tbl,engine,if_exists='append')
-		tsl.log(tbl + " done")
+		pl.log(tbl + " done")
 	except BaseException, e:
 		print e
-		tsl.log(tbl + " error")
+		pl.log(tbl + " error")
 	
-# no 2016 data ??
+# no data ??
 def shibor_quote(engine, year, sdate=None):
 	tbl = "shibor_quote"
-	tsl.log(tbl + " start...")
+	pl.log(tbl + " start...")
 	try:
 		df = ts.shibor_quote_data(year)
 		if sdate is not None:
 			df = df[df.date >= sdate]
 		df.to_sql(tbl,engine,if_exists='append')
-		tsl.log(tbl + " done")
+		pl.log(tbl + " done")
 	except BaseException, e:
 		print e
-		tsl.log(tbl + " error")
+		pl.log(tbl + " error")
 
 def shibor_ma(engine, year, sdate=None):
 	tbl = "shibor_ma"
-	tsl.log(tbl + " start...")
+	pl.log(tbl + " start...")
 	try:
 		df = ts.shibor_ma_data(year)
 		if sdate is not None:
 			df = df[df.date >= sdate]
 		df.to_sql(tbl,engine,if_exists='append')
-		tsl.log(tbl + " done")
+		pl.log(tbl + " done")
 	except BaseException, e:
 		print e
-		tsl.log(tbl + " error")
+		pl.log(tbl + " error")
 
 def lpr(engine, year, sdate=None):
 	tbl = "lpr"
-	tsl.log(tbl + " start...")
+	pl.log(tbl + " start...")
 	try:
 		df = ts.lpr_data(year)
 		if sdate is not None:
 			df = df[df.date >= sdate]
 		df.to_sql(tbl,engine,if_exists='append')
-		tsl.log(tbl + " done")
+		pl.log(tbl + " done")
 	except BaseException, e:
 		print e
-		tsl.log(tbl + " error")
+		pl.log(tbl + " error")
 	
 def lpr_ma(engine, year, sdate=None):
 	tbl = "lpr_ma"
-	tsl.log(tbl + " start...")
+	pl.log(tbl + " start...")
 	try:
 		df = ts.lpr_ma_data(year)
 		if sdate is not None:
 			df = df[df.date >= sdate]
 		df.to_sql(tbl,engine,if_exists='append')
-		tsl.log(tbl + " done")
+		pl.log(tbl + " done")
 	except BaseException, e:
 		print e
-		tsl.log(tbl + " error")
+		pl.log(tbl + " error")
 	
 def futures_ifs(engine, cdate):
 	tbl = "futures_ifs"
-	tsl.log(tbl + " start...")
+	pl.log(tbl + " start...")
 	try:
 		df = ts.get_intlfuture()
 		df['date'] = cdate
 		df.to_sql(tbl,engine,if_exists='append')
-		tsl.log(tbl + " done")
+		pl.log(tbl + " done")
 	except BaseException, e:
 		print e
-		tsl.log(tbl + " error")
+		pl.log(tbl + " error")
 
 def global_index(engine, cdate):
 	tbl = "global_index"
-	tsl.log(tbl + " start...")
+	pl.log(tbl + " start...")
 	try:
 		df = ts.global_realtime()
 		df['date'] = cdate
 		df.to_sql(tbl,engine,if_exists='append')
-		tsl.log(tbl + " done")
+		pl.log(tbl + " done")
 	except BaseException, e:
 		print e
-		tsl.log(tbl + " error")
+		pl.log(tbl + " error")
